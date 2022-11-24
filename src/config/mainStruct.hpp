fec7b8c80c323b9c3e1ab9bfdf9d161499d46d9a
AHMAD DIDIK SETIYADI <adidiksetiyadi@gmail.com>
2022-11-24 21:12:57 +0700

    backup

--- src/config/mainStruct.hpp
+++ src/config/mainStruct.hpp
@@ -1,59 +1,8 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
-
 typedef struct
 {
-    /* data */
-    uint32_t currentLength = 0;
-    uint32_t totalLength;
-    String currentFSVers = "0.1";
-    String currentFirmVers = "0.1";
-    uint8_t httpCode;
-    uint32_t len;
-} FS_Struct;
-
-typedef struct
-{
-    const char *urlFirm;
-    const char *urlFS;
-    const char *urlUpdate;
-    const char *Param_UrlFS = "UrlFS";
-    const char *Param_UrlFirm = "UrlFirm";
-    const char *Param_WiFiSSID = "WiFiSSID";
-    const char *Param_WiFiPass = "WiFiPass";
+    const char *firmwareUrl;
+    const char *fsUrl;
     const char *ssid;
     const char *pass;
-    const char *MacAddr;
-} Firm_Dasar_Struct;
-
-DynamicJsonDocument doc(64);
-DynamicJso#include <Arduino.h>
-#include <ArduinoJson.h>
-
-typedef struct
-{
-    /* data */
-    uint32_t currentLength = 0;
-    uint32_t totalLength;
-    String currentFSVers = "0.1";
-    String currentFirmVers = "0.1";
-    uint8_t httpCode;
-    uint32_t len;
-} FS_Struct;
-
-typedef struct
-{
-    const char *urlFirm;
-    const char *urlFS;
-    const char *urlUpdate;
-    const char *Param_UrlFS = "UrlFS";
-    const char *Param_UrlFirm = "UrlFirm";
-    const char *Param_WiFiSSID = "WiFiSSID";
-    const char *Param_WiFiPass = "WiFiPass";
-    const char *ssid;
-    const char *pass;
-    const char *MacAddr;
-} Firm_Dasar_Struct;
-
-DynamicJsonDocument doc(64);
-DynamicJsonDocument docVer(64);
+    bool state = false;
+} baseFirmwareStruct;
